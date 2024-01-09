eca2c722307f0500fb207fac7ea6d6944eaf8595
EvertonDepla <evertondeplaprogramador@gmail.com>
2024-01-09 13:10:23 -0300

    Mudamça no Java Script

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
- function generateNumber (){
+function generateNumber() {
 
- const min = Math.ceil(document.querySelector(".input-min").value)
- const max = Math.floor(document.querySelector(".input-max").value)
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
 
- const finalResult = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert("The minimum value must be LESS than the maximum value")
+    }
+    else {
+        const finalResult = Math.floor(Math.random() * (max - min + 1)) + min
 
- alert (finalResult)
+        alert(finalResult)
+    }
 }
